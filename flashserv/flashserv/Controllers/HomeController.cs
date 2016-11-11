748b54c0eb3e21cfa150d173bd0d1eaf54197bd0
Mihai Radulescu <mihai.radulescu@nerdscomputing.com>
2016-11-11 18:24:50 +0200

    go nerds go

---
+++ flashserv/flashserv/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web; 
+using System.Web.Mvc;
+
+namespace flashserv.Controllers
+{
+ 
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public string Index()
+        {
+            return "Code hard and have fun guys... first and first we need to have fun!!!!! now go make nerds proud";
+        }
+    }
+}
